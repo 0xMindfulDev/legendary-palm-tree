636f83684cd2628e2ee3aad39c25efdeb700c922
jimrox-console <jimroxodezi@gmail.com>
2022-01-30 04:14:49 +0100

    update chapter two

---
+++ DSA/chapter-02/exe30.py
@@ -0,0 +1,5 @@
+"""At the close of Section 2.4.1, we suggest a model in which the CreditCard
+class supports a nonpublic method, set balance(b), that could be used
+by subclasses to affect a change to the balance, without directly accessing
+the balance data member. Implement such a model, revising both the
+CreditCard and PredatoryCreditCard classes accordingly."""
