b0c71c0b426c4cdd7260643a0f1342f150df2c23
jimrox-console <jimroxodezi@gmail.com>
2022-01-30 04:28:16 +0100

    exercise twenty-two

--- DSA/chapter-01 - Python Basics/exe22.py
+++ DSA/chapter-01 - Python Basics/exe22.py
@@ -1,3 +1,18 @@
 """Write a short Python program that takes two arrays a and b of length n
 storing int values, and returns the dot product of a and b. That is, it returns
 an array c of length n such that c[i] = a[i] · b[i], for i = 0, . . . , n − 1."""
+
+from typing import List
+
+def dot_product(a: List[int], b: List):
+    c = []
+    for i in range(len(a)):
+        c.append((a[i]*b[i]))
+    return c
+
+
+#test program
+if __name__ == "__main__":
+    l1 = [1,2,3]
+    l2 = [1,2,3]
+    print(dot_product(l1, l2))
