009f3100a62ffa5d8585274117ff7705fe5f50d7
jimrox-console <jimroxodezi@gmail.com>
2022-01-31 17:58:53 +0100

    solved

--- DSA/chapter-01 - Python Basics/exe01.py
+++ DSA/chapter-01 - Python Basics/exe01.py
@@ -1,4 +1,18 @@
-"""Write a short Python function, is multiple(n, m), that takes two integer
+"""Write a short Python function, is_multiple(n, m), that takes two integer
 values and returns True if n is a multiple of m, that is, n = mi for some
 integer i, and False otherwise."""
 
+
+
+def is_multiple(n: int, m: int) -> bool:
+    """
+    function to determine of n is a multiple of m
+    """
+    return n % m == 0
+
+
+#test function
+if __name__ == '__main__':
+    print(is_multiple(20, 3))
+    print(is_multiple(21, 3))
+    print(is_multiple(77, 7))
