14556ff1d1ba38ed136acf03f6d2b61c851fb5ae
jimrox-console <jimroxodezi@gmail.com>
2022-01-31 17:06:13 +0100

    exercise twenty-two

--- DSA/chapter-01 - Python Basics/exe22.py
+++ DSA/chapter-01 - Python Basics/exe22.py
@@ -5,6 +5,10 @@ an array c of length n such that c[i] = a[i] · b[i], for i = 0, . . . , n − 1
 from typing import List
 
 def dot_product(a: List[int], b: List):
+
+    if len(a) != len(b):
+        raise ValueError("Both lists must of the same length")
+
     c = []
     for i in range(len(a)):
         c.append((a[i]*b[i]))
@@ -16,3 +20,7 @@ if __name__ == "__main__":
     l1 = [1,2,3]
     l2 = [1,2,3]
     print(dot_product(l1, l2))
+
+    c1 = [2,4,6]
+    c2 = [1,2,3,4]
+    print(dot_product(c1, c2))  #raises Error.
